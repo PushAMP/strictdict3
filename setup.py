2fdac2849bf3a2440984b7a2990e735c4a93ba7e
Dmitry Zhiltsov <dzhiltsov@me.com>
2015-10-05 18:56:36 +0300

    Switch to setuptools test integration

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
+import sys
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 pkgs = find_packages()
 
@@ -12,11 +34,14 @@ setup(
     author_email='dzhiltsov@me.com',
     ext_modules=[],
     packages=pkgs,
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
     scripts=[],
     data_files=[],
     install_requires=[
         'msgpack-python',
     ],
+    test_suite="strictdict.tests",
     platforms='any',
     license='MIT',
     url='https://github.com/PushAMP/strictdict3',
