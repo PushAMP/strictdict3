232dbdeeb4e99abbf2fa18501eda0af721e806ef
Dmitry Zhiltsov <dzhiltsov@me.com>
2015-10-21 12:50:38 +0300

    Update version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ pkgs = find_packages()
 
 setup(
     name='strictdict3',
-    version='0.4.3',
+    version='0.4.4',
     description='Strict Dict',
     author='Dmitry Zhiltsov',
     author_email='dzhiltsov@me.com',
